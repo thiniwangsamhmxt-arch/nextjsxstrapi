16df064c359c4c8d5b9c76266d4bd5d274f1330e
XXXIVAx <140949279+CakekritsananXi@users.noreply.github.com>
2025-11-03 10:30:41 +0700

    Updated i18n.config.ts

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,4 @@
 export const i18n = {
-  defaultLocale: 'en',
-  locales: ['en', 'fr'],
-} as const;
-
-export type Locale = (typeof i18n)['locales'][number];
+  defaultLocale: 'th',
+  locales: ['en', 'th', 'th-TH'], // ✅ ไม่ใช้ "th_TH" เด็ดขาด
+};
